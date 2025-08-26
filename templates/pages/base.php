ecd76b441d14a29b5fee82cf9728f6b112c2a362
steve <StevencNewbern@gmail.com>
2025-08-26 06:12:28 -0500

    8/26/2025

--- templates/pages/base.php
+++ templates/pages/base.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/jpg" href="templates/static/bg.jpg">
 
 
     <title><?php echo $title; ?></title>
